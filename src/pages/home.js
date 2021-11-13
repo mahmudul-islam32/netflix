4d0ebe0c83f138ae7d282bc13e0d066e6f186bb6
Mahmudul Islam <mahmud@iMac-von-Mahmudul.local>
2021-11-13 22:59:33 +0100

    make header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { OptForm } from '../components';
+import HeaderContainer from '../containers/header';
 import FooterContainer from '../containers/footer';
 import FaqsContainer from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
@@ -6,10 +8,19 @@ import { JumbotronContainer } from '../containers/jumbotron';
 function home(props) {
     return (
     <>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
-        
+     <HeaderContainer>
+     <OptForm>
+        <OptForm.Input placeholder="Email address" />
+               <OptForm.Button>Try it now</OptForm.Button>
+               <OptForm.Text> 
+                 Ready to watch? Enter your email to create or restart yor membership
+               </OptForm.Text>
+     </OptForm>
+     </HeaderContainer>
+     <JumbotronContainer />
+     <FaqsContainer />
+     <FooterContainer />
+    
     </>
     );
 }
